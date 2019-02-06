88fe20650b1254397f6baf477fcca64f41586aea
Gal Yaniv <galyaniv12@gmail.com>
2019-02-06 16:50:55 +0200

    added delete by id and get by id, changed response in create recipe

--- server/recipe/index.js
+++ server/recipe/index.js
@@ -6,8 +6,11 @@ const router = express.Router()
 router.post("/", (req, res) => {
     const recipe = new Recipe(req.body)
     recipe.save()
-        .then(data => res.send(`Saved!<br> recipe name: ${data.title}.<br> recipe id: ${data._id}`))
         .catch(e => res.status(400).send(e.message))
+        .then((data) => {
+            const response = { success: true, id: data._id }
+            res.send(response)
+        })
 })
 
 // get all recipes. if used with query in url gets only recipes with titles matching the query
@@ -25,6 +28,26 @@ router.get("/", (req, res) => {
     Recipe.find(filter)
         .then(data => res.send(data))
         .catch(e => res.status(400).send(e.message))
+})
+
+// get a recipe by id
+router.get("/:recipeId", (req, res) => {
+    Recipe.findById(req.params.recipeId)
+        .then(data => res.send(data))
+        .catch(e => res.status(400).send(e.message))
+})
+
+// delete recipe by id
+router.delete("/:recipeId", (req, res) => {
+    
+    let id = req.params.recipeId
+    
+    Recipe.findByIdAndRemove(id)
+        .catch(e => res.status(400).send(e.message))
+        .then(() => {
+            const response = { success: true, id: id }
+            res.send(response)
+        })
 })
 
 module.exports = router
