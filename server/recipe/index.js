ea982aa0375012e835c7e73af75ba032c55ca33a
Gal Yaniv <galyaniv12@gmail.com>
2019-02-06 16:39:42 +0200

    added get all request with query option. currently only searching titles.

--- server/recipe/index.js
+++ server/recipe/index.js
@@ -1,13 +1,30 @@
-const Recipe = require("./recipeModel");
-const express = require("express");
-const router = express.Router();
+const Recipe = require("./recipeModel")
+const express = require("express")
+const router = express.Router()
 
 // create recipe
 router.post("/", (req, res) => {
-    const recipe = new Recipe(req.body);
+    const recipe = new Recipe(req.body)
     recipe.save()
         .then(data => res.send(`Saved!<br> recipe name: ${data.title}.<br> recipe id: ${data._id}`))
-        .catch(e => res.status(400).send(e.message));
-});
+        .catch(e => res.status(400).send(e.message))
+})
 
-module.exports = router;
+// get all recipes. if used with query in url gets only recipes with titles matching the query
+router.get("/", (req, res) => {
+    let filter = {}
+    let query = req.query.q
+
+    if (query) {
+        console.log(query)
+        const rgx = new RegExp(query, "i")
+        console.log(rgx)
+        filter = { title: rgx }
+    }
+
+    Recipe.find(filter)
+        .then(data => res.send(data))
+        .catch(e => res.status(400).send(e.message))
+})
+
+module.exports = router
