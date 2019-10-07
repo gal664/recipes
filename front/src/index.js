692003c9eee57b846413c4381254d1828ff5c474
Gal Yaniv <galyaniv12@gmail.com>
2019-10-07 22:26:05 +0300

    updates

--- front/src/index.js
+++ front/src/index.js
@@ -4,8 +4,8 @@ import App from './components/App'
 import 'bootstrap/dist/css/bootstrap.css'
 import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.render(
+ReactDOM.render((
       <BrowserRouter>
             <App />
       </BrowserRouter>
-      , document.getElementById('root'))
+), document.getElementById('root'))
